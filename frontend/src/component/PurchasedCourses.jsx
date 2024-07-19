c674de1bbf1376ac6810e095737f5d28530f68a8
sanyam <sanyammadaanfca@gmail.com>
2024-07-19 22:36:51 +0530

    changes in purchase styling

--- frontend/src/component/PurchasedCourses.jsx
+++ frontend/src/component/PurchasedCourses.jsx
@@ -3,14 +3,13 @@ import React from "react";
 export function PurchasedCourses({ purchasedCourses }) {
     return (
         <div className="purchased_courses">
-            <h2>Your Purchased Courses</h2>
             <ul>
                 {purchasedCourses && purchasedCourses.length > 0 ? (
                     purchasedCourses.map(course => (
                         <li key={course._id}>{course.coursename}</li>
                     ))
                 ) : (
-                    <li>No purchased courses yet</li>
+                    <li className="text-red-800 text-2xl text-center mt-2 font-bold">No purchased courses yet</li>
                 )}
             </ul>
         </div>
